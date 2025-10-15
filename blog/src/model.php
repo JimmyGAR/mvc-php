2778957fed90ae89cfde4cbcd4f28b0313bda785
JimmyGAR <jimmy.garnier1110@gmail.com>
2025-10-15 16:47:42 +0200

    Isolez l'accès aux données (attention ! ancien push était sur Isolez l'affichage du traitement PHP)

--- blog/src/model.php
+++ blog/src/model.php
@@ -1,30 +1,27 @@
 <?php
 function getPosts()
 {
-    // We connect to the database.
+    // Connexion à la base de données
     try {
-        $database = new PDO(
-            'mysql:host=localhost;dbname=blog;charset=utf8',
-            'blog',
-            'password'
-        );
+        $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
     } catch (Exception $e) {
         die('Erreur : ' . $e->getMessage());
     }
-    // We retrieve the 5 last blog posts.
-    $statement = $database->query(
-        "SELECT id, titre, contenu, DATE_FORMAT(date_creation,'%d/%m/%Y à %Hh%imin%ss') AS date_creation_fr FROM billets
-ORDER BY date_creation DESC LIMIT 0, 5"
-    );
+
+    // On récupère les 5 derniers billets
+    $statement = $database->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
+
     $posts = [];
-    while (($row = $statement->fetch())) {
+    while ($row = $statement->fetch()) {
         $post = [
             'title' => $row['titre'],
-            'french_creation_date' => $row['date_creation_fr'],
             'content' => $row['contenu'],
+            'frenchCreationDate' => $row['date_creation_fr']
         ];
+
         $posts[] = $post;
     }
+
     return $posts;
 }
 ?>
